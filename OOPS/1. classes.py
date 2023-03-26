46b5d6d271f7534aecfac50153422527fd2de279
saquibmohiuddin <saquib.mohiuddin@hotmail.com>
2023-03-26 14:47:13 +0100

    basics of classes written

--- OOPS/1. classes.py
+++ OOPS/1. classes.py
@@ -1,10 +1,8 @@
+
+# %%
 import turtle
 
 car = turtle.Turtle()
 car.circle(radius=100)
 
-my_screen = Screen()
-my_screen.canvheight
-# class Car:
-#     def __init__(self) -> None:
-#         pass
+# %%
