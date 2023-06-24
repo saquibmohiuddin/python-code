9049634b4e31588831cef9721f028f4c8cdb7064
saquibmohiuddin <saquib.mohiuddin@hotmail.com>
2023-06-24 11:17:51 +0100

    insert sort added

---
+++ algorithms/algorithms.py
@@ -0,0 +1,25 @@
+# sorting problem
+# 1. insertion sort - small arrays
+# 2. merge sort
+# 3. quick sort
+# 4. heap sort
+# 5. Tim sort
+
+
+x = [21, 23, 20, 18, 4, 19, 45, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10]
+
+def insert_sort(x):
+    for i in 
+
+
+
+
+# def insertion_sort(x):
+#     for i in range(1, len(x)):
+#         j = i
+#         while j > 0 and x[j-1] > x[j]:
+#             x[j-1], x[j] = x[j], x[j-1]
+#             j -= 1
+#     return x
+
+# print(insertion_sort(x))
