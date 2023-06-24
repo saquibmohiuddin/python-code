172b1edc5707eac4918022ae766aee42a38a1837
saquibmohiuddin <saquib.mohiuddin@hotmail.com>
2023-06-24 13:12:18 +0100

    insert sort algo written

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -8,18 +8,12 @@
 
 x = [21, 23, 20, 18, 4, 19, 45, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10]
 
-def insert_sort(x):
-    for i in 
+def insertion_sort(x):
+    for i in range(1, len(x)):
+        j = i
+        while j > 0 and x[j-1] > x[j]:
+            x[j-1], x[j] = x[j], x[j-1]
+            j -= 1
+    return x
 
-
-
-
-# def insertion_sort(x):
-#     for i in range(1, len(x)):
-#         j = i
-#         while j > 0 and x[j-1] > x[j]:
-#             x[j-1], x[j] = x[j], x[j-1]
-#             j -= 1
-#     return x
-
-# print(insertion_sort(x))
+print(insertion_sort(x))
